22e32a120e5d82344d15fc72a0f2ba4f3895dc1c
Raquel Fernandes <77596704+RaqFern@users.noreply.github.com>
2023-08-31 23:49:46 -0300

    Add file
    Programa identificando quantos dias tem um determinado mês, usando switch case.

---
+++ HowManyDays.java
@@ -0,0 +1,47 @@
+
+package myprojects.projetoexercicios;
+
+public class HowManyDays {
+
+    public static void main(String[] args) {
+        int month = 4;
+        
+        switch (month) {
+            case 1:   
+
+            case 3:  
+
+            case 5:
+
+            case 7:
+
+            case 8:
+
+            case 10:
+
+            case 12:
+                
+                System.out.println("31 days");
+                break;
+                
+            case 2: 
+                
+                System.out.println("28 days");
+                break;
+            
+            case 4:
+
+            case 6:
+
+            case 9:
+
+            case 11:
+                
+                System.out.println("30 days");
+                break;
+                
+            default: System.out.println("Error: Pick a month, 1 to 12");
+        }
+    }
+    
+}
