392461c0e8e7c5896db761ab929e972915e7a5c1
Raquel Fernandes <77596704+RaqFern@users.noreply.github.com>
2023-08-31 23:48:37 -0300

    Add file
    Treinando o uso do switch case

---
+++ ProjetoExercicios2.java
@@ -0,0 +1,20 @@
+package myprojects.projetoexercicios;
+
+public class ProjetoExercicios2 {
+
+   
+    public static void main(String[] args) {
+       
+        int codigoProduto = 2;
+        
+        switch(codigoProduto) {
+            case 1:              
+                System.out.println("Produto do fornecedor A");
+                break;
+                
+            case 2:
+                System.out.println("Produto do fornecedor B");    
+                break;
+        }     
+    }
+}
