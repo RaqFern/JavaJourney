59911bf20f6e9c01b7a7881dc6567f2a2013e210
Raquel Fernandes <77596704+RaqFern@users.noreply.github.com>
2023-09-05 23:24:36 -0300

    Add
    Introduction to for loops // introdução ao for loop

---
+++ ProjetoExercicios5.java
@@ -0,0 +1,14 @@
+
+package myprojects.projetoexercicios;
+
+public class ProjetoExercicios5 {
+
+    public static void main(String[] args) {
+        /* criando a variável i, inicializando em 0; enquanto i for <= 10 o 
+        código vai ficar rodando; incrementando a variável; : for (int i = 0; i <=10; i++), ou: */ 
+        int i = 0;
+        for (; i <= 10; i++) {
+        }    
+    }
+    
+}
