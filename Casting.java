6facb96054ed47fdd2cf563a951c3dd14bd6032f
Raquel Fernandes <77596704+RaqFern@users.noreply.github.com>
2023-09-15 23:08:53 -0300

    Add files via upload
    Practing casting

---
+++ Casting.java
@@ -0,0 +1,30 @@
+
+package myprojects.cursoprogramacao;
+
+public class Casting {
+        public static void main(String[] args) {
+        
+    double b, B, h, area;
+    
+    b = 6.0;
+    B = 8.0;        
+    h = 5.0;
+            
+    area = (b + B) / 2.0 * h;
+    
+        System.out.println(area);
+    
+    int c, d;
+    double resultado;
+        
+    c = 5;
+    d = 2;
+    
+    resultado = (double) c / d;
+    
+        System.out.println(resultado);
+    
+   
+    }
+}
+
